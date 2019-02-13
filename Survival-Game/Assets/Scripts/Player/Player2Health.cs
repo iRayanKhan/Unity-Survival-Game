f9be292f9748ba3e4ea309e4715f0716c0862d02
Rayan Khan <khanrayan7088@gmail.com>
2019-02-12 18:51:44 -0600

    Project File
    Added Project Files

---
+++ Survival-Game/Assets/Scripts/Player/Player2Health.cs
@@ -0,0 +1,89 @@
+﻿ using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Player2Health : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    Player2Movement player2Movement;
+    Player2Shooting player2Shooting;
+    bool isDead;
+    bool damaged;
+
+
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();
+        playerAudio = GetComponent <AudioSource> ();
+        player2Movement = GetComponent <Player2Movement> ();
+        player2Shooting = GetComponentInChildren <Player2Shooting> ();
+
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play ();
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+
+        player2Shooting.DisableEffects ();
+
+        anim.SetTrigger ("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+
+        player2Movement.enabled = false;
+        player2Shooting.enabled = false;
+    }
+
+
+    public void RestartLevel ()
+    {
+        SceneManager.LoadScene (0);
+    }
+}
---
+++ Survival-Game/Assets/Scripts/Player/Player2Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player2Movement : MonoBehaviour
+{ 
+    public float Speed = 3.0F;
+    public float RotateSpeed = 3.0F;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (transform != null)
+        {
+            transform.Rotate(0, Input.GetAxisRaw("Horizontal 2") * RotateSpeed, 0);
+            var forward = transform.TransformDirection(Vector3.forward);
+            float curSpeed = Speed * Input.GetAxisRaw("Vertical 2");
+            controller.SimpleMove(forward * curSpeed);
+        }
+    }
+        void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal 2");
+        float v = Input.GetAxisRaw("Vertical 2");
+        
+        Animating(h, v);
+
+    }
+
+   
+       
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+}
